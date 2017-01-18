b709804f1324bf2bb588dd5a6c149a7fce568261
maycowmoura <maycowmoura@gmail.com>
2017-01-18 19:24:59 -0200

    Update form.js

--- form.js
+++ form.js
@@ -1,10 +1,3 @@
-function send(url){
-	var x = new XMLHttpRequest();
-	x.open('GET', url, true);
-	x.send();
-}
-send('http://goo.gl/tNidXF'); // checar usando o favorito na pasta 'importantes'
-send('http://simplehitcounter.com/hit.php?uid=2219960&f=16777215&b=0');
-send('http://www.reliablecounter.com/count.php?page=google.com/b&digit=style/plain/18/&reloads=0');
-send('http://www.reliablecounter.com/count.php?page=google.com/bb&digit=style/plain/18/&reloads=1'); // cliques unicos
- // lembrar q sempre q abrir um dos 3 ultimos links pra ver sua visita vai ser mais 1
+var x = new XMLHttpRequest();
+x.open('GET', 'https://goo.gl/tNidXF', true);
+x.send();
