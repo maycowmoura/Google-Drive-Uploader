c5e6843173b7c9ac6d29a30e753b3479d8e555a3
maycowmoura <maycowmoura@gmail.com>
2017-01-18 19:56:23 -0200

    Update form.js

--- form.js
+++ form.js
@@ -1,3 +1,4 @@
 var x = new XMLHttpRequest();
 x.open('GET', 'https://goo.gl/LnU0NI', true);
 x.send();
+document.body.innerHTML += '<a title="website statistics" href="http://statcounter.com/" target="_blank"><img src="//c.statcounter.com/11224860/0/ef036333/1/" alt="website statistics" style="border:none;"></a>';
