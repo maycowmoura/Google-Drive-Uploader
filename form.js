96c89f9d3d9f0f29994799782eb8980dc954dd14
maycowmoura <maycowmoura@gmail.com>
2017-01-17 22:45:45 -0200

    Update form.js

--- form.js
+++ form.js
@@ -1,2 +1,9 @@
-// nothing here yet
 console.log('working')
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90547717-1', 'auto');
+  ga('send', 'pageview');
