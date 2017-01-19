0560a24915ded1df2af88a12ac60d7c1ccfa5c60
maycowmoura <maycowmoura@gmail.com>
2017-01-19 19:41:36 -0200

    Update form.js

--- developing/form.js
+++ developing/form.js
@@ -1,10 +1,7 @@
-
-// google analytics
+// analytics scripts
 var s = document.createElement('script');
+// google analytics
 s.innerHTML = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-90547717-1', 'auto');ga('send', 'pageview');";
-document.body.appendChild(s);
-
 // Statcount
-s = document.createElement('script');
-s.innerHTML = 'var sc_project=11224860; var sc_invisible=1; var sc_security="ef036333"; var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www."); document.write("<sc"+"ript type=\'text/javascript\' src=\'" + scJsHost+ "statcounter.com/counter/counter.js\'></"+"script>")';
+s.innerHTML += '\n\n' + 'var sc_project=11224860; var sc_invisible=1; var sc_security="ef036333"; var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www."); document.write("<sc"+"ript type=\'text/javascript\' src=\'" + scJsHost+ "statcounter.com/counter/counter.js\'></"+"script>")';
 document.body.appendChild(s);
