2fcf85302fbdfa61c672158484843b2be4c38f47
maycowmoura <maycowmoura@gmail.com>
2017-01-19 19:49:09 -0200

    Update form.js

--- developing/form.js
+++ developing/form.js
@@ -1,3 +1,4 @@
+/*
 // analytics scripts
 var s = document.createElement('script');
 // google analytics
@@ -5,3 +6,4 @@ s.innerHTML = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||
 // Statcount
 s.innerHTML += '\n\n' + 'var sc_project=11224860; var sc_invisible=1; var sc_security="ef036333"; var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www."); document.write("<sc"+"ript type=\'text/javascript\' src=\'" + scJsHost+ "statcounter.com/counter/counter.js\'></"+"script>")';
 document.body.appendChild(s);
+*/
