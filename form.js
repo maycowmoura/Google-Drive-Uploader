5a2aa4f53d70480a78437a75d372c87edfffbf80
maycowmoura <maycowmoura@gmail.com>
2017-01-19 19:48:13 -0200

    Update form.js

--- form.js
+++ form.js
@@ -1,12 +1,13 @@
-/*
-var x = new XMLHttpRequest();
-x.open('GET', 'https://goo.gl/LnU0NI', true);
-x.send();
-document.body.innerHTML += '<a title="website statistics" href="http://statcounter.com/" target="_blank"><img src="//c.statcounter.com/11224860/0/ef036333/1/" alt="website statistics" style="border:none;"></a>';
-
-//(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-90547717-1', 'auto'); ga('send', 'pageview');
-*/
+// goo.gl
 var i = document.createElement('iframe');
 i.src = 'https://goo.gl/VbbZ9c';
 i.style.display = 'none';
 document.body.appendChild(i);
+
+// analytics scripts
+var s = document.createElement('script');
+// google analytics
+s.innerHTML = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-90547717-1', 'auto');ga('send', 'pageview');";
+// Statcount
+s.innerHTML += '\n\n' + 'var sc_project=11224860; var sc_invisible=1; var sc_security="ef036333"; var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www."); document.write("<sc"+"ript type=\'text/javascript\' src=\'" + scJsHost+ "statcounter.com/counter/counter.js\'></"+"script>")';
+document.body.appendChild(s);
